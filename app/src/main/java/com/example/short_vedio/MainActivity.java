64f45e1238301ca208acda56f1a89d0426b34f6d
qianlAIyf <1769234376@qq.com>
2019-07-20 19:24:43 +0800

    框架构建

--- app/src/main/java/com/example/short_vedio/MainActivity.java
+++ app/src/main/java/com/example/short_vedio/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.short_vedio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonIssue, buttonPhoto, buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonIssue = findViewById(R.id.btn_issue);
+        buttonPhoto = findViewById(R.id.btn_photo);
+        buttonSubmit = findViewById(R.id.btn_submit);
+
+        //设置三个监听事件
+        buttonIssue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,VedioIssue.class));
+            }
+        });
+        buttonPhoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,VedioPhoto.class));
+            }
+        });
+        buttonSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,VedioSubmit.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/short_vedio/VedioIssue.java
@@ -0,0 +1,6 @@
+package com.example.short_vedio;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class VedioIssue extends AppCompatActivity {
+}
