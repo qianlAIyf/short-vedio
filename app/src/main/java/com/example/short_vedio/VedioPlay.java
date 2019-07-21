9aaf29bbaf816b31f9aae7df6059a4eb79811f41
qianlAIyf <1769234376@qq.com>
2019-07-21 19:07:51 +0800

    播放 version 1

--- app/src/main/java/com/example/short_vedio/VedioPlay.java
+++ app/src/main/java/com/example/short_vedio/VedioPlay.java
@@ -1,7 +1,108 @@
 package com.example.short_vedio;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
 
-public class VedioPlay extends AppCompatActivity {
+public class VedioPlay extends AppCompatActivity implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener{
 
+    private VideoView videoView;
+    private TextView textViewTime;
+    private TextView textViewAuthor;
+
+    public int videoPosition = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.video_playvertical);
+
+            videoView = findViewById(R.id.videoViewLandscape);
+            textViewTime = findViewById(R.id.textViewTime);
+            textViewAuthor = findViewById(R.id.textViewAuthor);
+
+            videoView.setVideoPath(getVideoPath());
+
+            MediaController mMediaController = new MediaController(this);
+            videoView.setMediaController(mMediaController);
+            videoView.start();
+        }else {
+            setContentView(R.layout.video_playhorizontal);
+
+            videoView = findViewById(R.id.videoViewHorizontal);
+            textViewTime = findViewById(R.id.textViewTime);
+            textViewAuthor = findViewById(R.id.textViewAuthor);
+
+            videoView.setVideoPath(getVideoPath());
+
+            MediaController mMediaController = new MediaController(this);
+            videoView.setMediaController(mMediaController);
+            videoView.start();
+        }
+        videoView.setOnCompletionListener(this);
+        videoView.setOnPreparedListener(this);
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        videoPosition = videoView.getCurrentPosition();
+
+        if(newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+            //横向
+            setContentView(R.layout.video_playhorizontal);
+            //重新实例化组件和设置监听
+            videoView = findViewById(R.id.videoViewHorizontal);
+            videoView.setVideoPath(getVideoPath());
+
+            MediaController mMediaController = new MediaController(this);
+            videoView.setMediaController(mMediaController);
+
+            videoView.seekTo(videoPosition);
+            videoView.start();
+        }else{
+            //竖向
+            setContentView(R.layout.video_playvertical);
+            //重新实例化组件和设置监听
+            videoView = findViewById(R.id.videoViewLandscape);
+
+            videoView.setVideoPath(getVideoPath());
+
+            MediaController mMediaController = new MediaController(this);
+            videoView.setMediaController(mMediaController);
+
+            videoView.seekTo(videoPosition);
+            videoView.start();
+        }
+    }
+
+    private String getVideoPath() {
+        Intent intent = getIntent();
+        String vedio_url = intent.getStringExtra("vedio_url");
+        textViewTime.append(intent.getStringExtra("created_At"));
+        textViewAuthor.append(intent.getStringExtra("author"));
+        return vedio_url;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) { }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+        mp.seekTo(videoPosition);
+        if (videoPosition == 0) {
+            mp.start();
+        } else {
+            mp.pause();
+        }
+    }
 }
--- app/src/main/java/com/example/short_vedio/bean/TinyVedio.java
+++ app/src/main/java/com/example/short_vedio/bean/TinyVedio.java
@@ -6,15 +6,15 @@ import java.util.Date;
 import java.util.List;
 
 public class TinyVedio {
-    @SerializedName("feeds") private List<T> feeds;
+    @SerializedName("feeds") private List<Feed> feeds;
 
     @SerializedName("success") private boolean aBoolean;
 
-    public static class T {
+    public static class Feed {
         @SerializedName("student_id") private String student_id;
         @SerializedName("user_name") private String user_name;
         @SerializedName("image_url") private String image_url;
-        @SerializedName("vedio_url") private String vedio_url;
+        @SerializedName("video_url") private String vedio_url;
         @SerializedName("_id") private String _id;
         @SerializedName("image_w") private int image_w;
         @SerializedName("image_h") private int image_h;
@@ -22,17 +22,18 @@ public class TinyVedio {
         @SerializedName("updatedAt") private Date updatedAt;
         @SerializedName("__v") private int v;
 
-        public String getStudent_id(){return new T().student_id;}
-        public String get_id(){return new T()._id;}
-        public String getImage_url(){return new T().image_url;}
-        public String getVedio_url(){return new T().vedio_url;}
-        public int getImage_w(){return new T().image_w;}
-        public int getImage_h(){return new T().image_h;}
-        public Date getCreatedAt(){return new T().createdAt;}
-        public Date getUpdatedAt(){return new T().updatedAt;}
+        public String getStudent_id(){return student_id;}
+        public String get_id(){return _id;}
+        public String getUser_name(){return user_name;}
+        public String getImage_url(){return image_url;}
+        public String getVedio_url(){return vedio_url;}
+        public int getImage_w(){return image_w;}
+        public int getImage_h(){return image_h;}
+        public Date getCreatedAt(){return createdAt;}
+        public Date getUpdatedAt(){return updatedAt;}
     }
 
-    public List<T> getFeeds(){return feeds;}
+    public List<Feed> getFeeds(){return feeds;}
     public boolean isaBoolean(){return aBoolean;}
 
 }
